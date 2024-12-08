c57497a44775b83cb836bfaedfad2a6659755e6e
Himanshu Atwal <hematwal.2003@gmail.com>
2024-12-08 20:56:28 +0530

    updated

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
+import logger from './logger.js';
+import morgan from 'morgan';
 
 const app=express();
 
@@ -9,6 +11,26 @@ const port=process.env.PORT || 3000; // for deployment process
 
 app.use(express.json()); // data as JSON() formate
 
+//Logger creation---------->
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
+
 //<--------------------Crud Operations----------------------->
 
 let teaData=[]
@@ -17,6 +39,7 @@ let index=1;
 
 // storing new data--->
 app.post('/teas',(req,res)=>{
+    logger.info("A post was made to add new tea");
     const {name ,price}=req.body;
     let newTea={
         "id":index++,
